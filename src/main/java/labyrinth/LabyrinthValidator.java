b2760ef6b736df11f076cda57bae3f3503496dc2
bink81 <bink81@gmail.com>
2016-09-17 20:51:21 +0200

    Validate whether every cell is reachable

---
+++ src/main/java/labyrinth/LabyrinthValidator.java
@@ -0,0 +1,31 @@
+package labyrinth;
+
+import java.util.List;
+
+public class LabyrinthValidator {
+	// implementation of a graph algorithm to count connected components
+	public static int numberOfDisjointSections(final List<Integer>[] adjencyMatrix) {
+		boolean[] isNodeVisited = new boolean[adjencyMatrix.length];
+		int[] alocatedSections = new int[adjencyMatrix.length];
+		int currentSectionNumber = 0;
+		for (int node = 0; node < adjencyMatrix.length; node++) {
+			if (!isNodeVisited[node]) {
+				explore(node, adjencyMatrix, isNodeVisited, currentSectionNumber, alocatedSections);
+				currentSectionNumber++;
+			}
+		}
+		return currentSectionNumber;
+	}
+
+	private static void explore(final int parentNode, final List<Integer>[] adjencyMatrix,
+			final boolean[] isNodeVisited, final int currentSection, final int[] alocatedSections) {
+		isNodeVisited[parentNode] = true;
+		alocatedSections[parentNode] = currentSection;
+		List<Integer> neighborNodes = adjencyMatrix[parentNode];
+		for (int node : neighborNodes) {
+			if (!isNodeVisited[node]) {
+				explore(node, adjencyMatrix, isNodeVisited, currentSection, alocatedSections);
+			}
+		}
+	}
+}
---
+++ src/test/java/labyrinth/LabyrinthValidatorTest.java
@@ -0,0 +1,75 @@
+package labyrinth;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LabyrinthValidatorTest {
+
+	@Test
+	public void testNumberOfDisjointSections1() throws Exception {
+		int numberOfDisjointSections = process("c1.txt");
+
+		Assert.assertTrue(numberOfDisjointSections == 2);
+	}
+
+	@Test
+	public void testNumberOfDisjointSections2() throws Exception {
+		int numberOfDisjointSections = process("c2.txt");
+
+		Assert.assertTrue(numberOfDisjointSections == 4);
+	}
+
+	@Test
+	public void testNumberOfDisjointSections3() throws Exception {
+		int numberOfDisjointSections = process("c3.txt");
+
+		Assert.assertTrue(numberOfDisjointSections == 1);
+	}
+
+	/**
+	 * @param fileName
+	 *            the name of a file with a graph representation
+	 * 
+	 *            File format: the first line contains a number of vertices n
+	 *            and edges m. The following lines contain connected vertices u
+	 *            and v. Constraints: 2 <= n; 1 <= m; 1 <= u, v <= n; u != v
+	 * 
+	 * @return the number of disjoint sections
+	 */
+	private int process(String fileName) {
+		File file = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator + "graphs"
+				+ File.separator + fileName);
+
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+			int n = scanner.nextInt();
+			int m = scanner.nextInt();
+			List<Integer>[] adj = new ArrayList[n];
+			for (int i = 0; i < n; i++) {
+				adj[i] = new ArrayList<Integer>();
+			}
+			for (int i = 0; i < m; i++) {
+				int x, y;
+				x = scanner.nextInt();
+				y = scanner.nextInt();
+				adj[x - 1].add(y - 1);
+				adj[y - 1].add(x - 1);
+			}
+			return LabyrinthValidator.numberOfDisjointSections(adj);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return 666;
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+	}
+}
