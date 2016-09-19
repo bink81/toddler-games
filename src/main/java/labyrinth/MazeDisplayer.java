bf68d9a884568dccd8e5edbc023d92352ab933c3
bink81 <bink81@gmail.com>
2016-09-19 19:21:05 +0200

    Swap characters for displayed areas

--- src/main/java/labyrinth/MazeDisplayer.java
+++ src/main/java/labyrinth/MazeDisplayer.java
@@ -14,7 +14,7 @@ public class MazeDisplayer {
 	public void print() {
 		for (int i = 0; i < maze.length; i++) {
 			for (int j = 0; j < maze[0].length; j++) {
-				System.out.print(maze[i][j] ? "X" : " ");
+				System.out.print(maze[i][j] ? " " : "X");
 			}
 			System.out.println();
 		}
