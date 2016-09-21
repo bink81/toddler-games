a8e54f9e93e8ee67e029c0291303cd4873af7c45
bink81 <bink81@gmail.com>
2016-09-21 19:22:50 +0200

    Add tests and documentation

--- src/main/java/labyrinth/MazeGenerator.java
+++ src/main/java/labyrinth/MazeGenerator.java
@@ -2,12 +2,28 @@ package labyrinth;
 
 import java.util.Random;
 
+import com.google.common.base.Preconditions;
+
 public class MazeGenerator {
 	private final int horizontalSize;
 	private final int verticalSize;
 	private final int roomToWallRatio;
 
+	/**
+	 * @param horizontalSize
+	 *          - positive integer
+	 * @param verticalSize
+	 *          - positive integer
+	 * @param roomToWallRatio
+	 *          - positive integer which determines how many walls and rooms the
+	 *          maze should contain; 1 is an equal ratio (50%) between walls and
+	 *          rooms, with 2 rooms appear 66% of time, with 3 - 75%, etc...
+	 */
 	public MazeGenerator(int horizontalSize, int verticalSize, int roomToWallRatio) {
+		Preconditions.checkArgument(horizontalSize >= 0, "roomToWallRatio must not be negative");
+		Preconditions.checkArgument(verticalSize >= 0, "roomToWallRatio must not be negative");
+		Preconditions.checkArgument(roomToWallRatio > 0, "roomToWallRatio must be positive");
+
 		this.horizontalSize = horizontalSize;
 		this.verticalSize = verticalSize;
 		this.roomToWallRatio = roomToWallRatio;
--- src/test/java/labyrinth/MazeGeneratorTest.java
+++ src/test/java/labyrinth/MazeGeneratorTest.java
@@ -1,12 +1,67 @@
 package labyrinth;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MazeGeneratorTest {
+	private static final int ALMOST_NO_WALLS_RATIO = Integer.MAX_VALUE;
+	private static final int ONLY_WALL_RATIO = 1;
+	private static final int DUMMY_RATIO = 2;
+	private static final int INVALID_RATIO = 0;
+	private static final int INVALID_SIZE = -1;
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGenerateWithIncorrectRatio() throws Exception {
+		new MazeGenerator(0, 0, INVALID_RATIO).generate();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGenerateWithIncorrectSizeX() throws Exception {
+		new MazeGenerator(INVALID_SIZE, 0, DUMMY_RATIO).generate();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGenerateWithIncorrectSizeY() throws Exception {
+		new MazeGenerator(0, INVALID_SIZE, DUMMY_RATIO).generate();
+	}
+
+	@Test
+	public void testGenerateEmpty() throws Exception {
+		boolean[][] maze = new MazeGenerator(0, 0, DUMMY_RATIO).generate();
+
+		Assert.assertTrue(maze.length == 0);
+	}
+
+	@Test
+	public void testGenerateOneWall() throws Exception {
+		boolean[][] maze = new MazeGenerator(1, 1, ONLY_WALL_RATIO).generate();
+
+		Assert.assertTrue(!maze[0][0]);
+	}
+
+	@Test
+	public void testGenerateThousandsWalls() throws Exception {
+		boolean[][] maze = new MazeGenerator(100, 1000, ONLY_WALL_RATIO).generate();
+
+		for (int i = 0; i < maze.length; i++) {
+			for (int j = 0; j < maze[0].length; j++) {
+				Assert.assertTrue(!maze[i][j]);
+			}
+		}
+	}
 
 	@Test
 	public void testGenerate() throws Exception {
-		new MazeGenerator(10, 20, 1).generate();
-	}
+		boolean[][] maze = new MazeGenerator(10, 20, ALMOST_NO_WALLS_RATIO).generate();
 
+		boolean roomEncountered = false;
+		for (int i = 0; i < maze.length && !roomEncountered; i++) {
+			for (int j = 0; j < maze[0].length && !roomEncountered; j++) {
+				if (maze[i][j]) {
+					roomEncountered = true;
+				}
+			}
+		}
+		Assert.assertTrue(roomEncountered);
+	}
 }
